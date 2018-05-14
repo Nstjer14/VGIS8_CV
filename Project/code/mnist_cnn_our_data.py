51ccdcead69970503ceba2e39c5dc5eaa646fdbd
Kongskrald <pcsprog@hotmail.com>
2018-05-14 12:01:46 +0200

    shitfix

--- Project/code/mnist_cnn_our_data.py
+++ Project/code/mnist_cnn_our_data.py
@@ -142,7 +142,8 @@ num_classes = NuniqueClasses
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
                  activation='relu',
-                 input_shape=input_shape))
+                 input_shape=input_shape,
+                 data_format='channels_first'))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
