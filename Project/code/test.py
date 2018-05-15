1b4ab657cbfc646f70ec585afcf40b21768aca03
Kongskrald <pcsprog@hotmail.com>
2018-05-15 11:59:01 +0200

    Fixed dimension issue

--- Project/code/test.py
+++ Project/code/test.py
@@ -16,35 +16,17 @@ from keras import backend as K
 batch_size = 128
 num_classes = 10
 epochs = 250
-'''
-# input image dimensions
-img_rows, img_cols = 28, 28
 
-# the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+def calculateConvOutputDims(width,filter_size,stride,padding):
+    # W = input size of image
+    # F = size of the filter
+    # S = stride
+    # P = amount of zero padding around the image
+    # K = depth of the conv layer
+    return (width-filter_size + 2*padding)//stride + 1
 
-if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+#print (calculateConvOutputDims(width=64,filter_size=3,stride=1,padding=1))
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-
-'''
 #%%
 import matplotlib.pyplot as plt
 import load_images_2_python
@@ -151,30 +133,34 @@ model.add(Conv2D(6, kernel_size=(3, 3),
                  input_shape=input_shape,
                  data_format='channels_first',
                  padding = "same"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(MaxPooling2D(pool_size=(2, 2),data_format='channels_first'))
 #model.add(Dropout(0.25))
 model.add(Conv2D(32,
                  kernel_size=(5, 5),
                  activation='relu',
-                 padding = "same"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+                 padding = "same",
+                 data_format='channels_first'))
+model.add(MaxPooling2D(pool_size=(2, 2),data_format='channels_first'))
 
 model.add(Conv2D(36,
                  kernel_size=(5, 5),
                  activation='relu',
-                 padding = "same"))
+                 padding = "same",
+                 data_format='channels_first'))
 model.add(MaxPooling2D(pool_size=(2, 2),data_format='channels_first'))
 
 model.add(Conv2D(150,
                  kernel_size=(5, 5),
                  activation='relu',
-                 padding = "same"))
+                 padding = "same",
+                 data_format='channels_first'))
 
-#
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax',))
+
+#model.get_config()
 
 #model.compile(loss=keras.losses.categorical_crossentropy,
 #              optimizer=keras.optimizers.Adadelta(),
@@ -185,14 +171,16 @@ adagrad = keras.optimizers.Adagrad(lr=learningrate, epsilon=None, decay=0.0005)
 model.compile(loss='categorical_crossentropy',
               optimizer=adagrad,
               metrics=['accuracy'])
-
+model.summary()
+'''
 history = model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           verbose=1,
           validation_split=0.1)
+'''
 score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0]*100)
+print('Test loss:', score[0])
 print('Test accuracy:', score[1]*100)
 
 
